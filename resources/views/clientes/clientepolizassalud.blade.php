69e3c946d43eb5156dd1af3ca74221e9a08f7d1d
rojasluis836@gmail.com <rojasluis836@gmail.com>
2024-01-29 16:58:04 -0400

    vistas pagos, siniestros, datos parte 3

--- resources/views/clientes/clientepolizassalud.blade.php
+++ resources/views/clientes/clientepolizassalud.blade.php
@@ -21,9 +21,10 @@
                             id="flush-headingp_{{$poliza->idinsurers}}"
                             class="accordion-header collapsed redondear"
                             data-bs-toggle="collapse" 
-                            data-bs-target="#flush-collapsePoliza_{{$poliza->idinsurers}}" 
+                            data-bs-target="#collapsePoliza_{{$poliza->idinsurers}}" 
                             aria-expanded="false" 
-                            aria-controls="flush-collapsePoliza_{{$poliza->idinsurers}}"
+                            aria-controls="collapsePoliza_{{$poliza->idinsurers}}"
+                            onclick="clickpoliza({{$poliza->idinsurers}})"
                             >
                             <img 
                               class="w-10" 
@@ -34,7 +35,7 @@
                                 {{ $poliza->name }}  {{ number_format($poliza->coverage, 2, ',', '.') }} USD
                         </h6>     
                         <div 
-                            id="flush-collapsePoliza_{{$poliza->idinsurers}}" 
+                            id="collapsePoliza_{{$poliza->idinsurers}}" 
                             class="accordion-collapse collapse" 
                             aria-labelledby="flush-headingp_{{$poliza->idinsurers}}" 
                             data-bs-parent="#acoordeoplolizasclientesalud"
@@ -117,76 +118,82 @@
                                             id="flush-headingnotas" 
                                             class="accordion-header collapsed redondear-2 collapsed" 
                                             data-bs-toggle="collapse" 
-                                            data-bs-target="#flush-collapseinfonotas" 
+                                            data-bs-target="#collapseinfonotas_{{$poliza->idinsurers}}" 
                                             aria-expanded="false" 
-                                            aria-controls="flush-collapseinfonotas"
+                                            aria-controls="collapseinfonotas_{{$poliza->idinsurers}}"
+                                            onclick="clicknota({{$poliza->idinsurers}})"
                                             >
                                             Notas
                                           </h6>
-                                          <div id="flush-collapseinfonotas" class="accordion-collapse collapse" aria-labelledby="flush-headingnotas" data-bs-parent="#accordionnotas" style="">
+                                          <div 
+                                            id="collapseinfonotas_{{$poliza->idinsurers}}" 
+                                            class="accordion-collapse collapse" 
+                                            aria-labelledby="flush-headingnotas" 
+                                            data-bs-parent="#accordionnotas"
+                                            >
                                             <div class="accordion-body row">
-                                              <!--  --> 
-                                              <div class ="card m-2">
-                                                <h6  style ="color:green !important;">
-                                                  Patologias Declaradas
-                                                </h6>
-                                                <?php
-                                                  $patologias =  DB::table('patologia')
-                                                  ->where('pat_id_poliza',$poliza->id_insurancepolicies)
-                                                  ->where('pat_status','=',1)->get();
-                                                  if ( $patologias->count() >0)
-                                                  {
-                                                    foreach ($patologias as $patologia => $p)
+                                                <!--  --> 
+                                                <div class ="card m-2">
+                                                  <h6  style ="color:green !important;">
+                                                    Patologias Declaradas
+                                                  </h6>
+                                                  <?php
+                                                    $patologias =  DB::table('patologia')
+                                                    ->where('pat_id_poliza',$poliza->id_insurancepolicies)
+                                                    ->where('pat_status','=',1)->get();
+                                                    if ( $patologias->count() >0)
                                                     {
-                                                      
-                                                      ?>
-                                                          <p>
-                                                          <?php
-                                                            echo $p->pat_descripcion; 
-                                                          ?>
-                                                          </p>
-                                                            
-                                                      <?php
-                                                    }
+                                                      foreach ($patologias as $patologia => $p)
+                                                      {
+                                                        
+                                                        ?>
+                                                            <p>
+                                                            <?php
+                                                              echo $p->pat_descripcion; 
+                                                            ?>
+                                                            </p>
+                                                              
+                                                        <?php
+                                                      }
 
-                                                  }
-                                                  else
-                                                  {
-                                                    echo " Ninguno ";
-                                                  }
-                                                ?>
-                                              </div>
-                                              <div class ="card m-2">
-                                                <h6 style ="color:#911d1b !important;">
-                                                  Patologias NO Declaradas
-                                                </h6>
-                                                <?php
-                                                  $patologiasno =  DB::table('patologia')
-                                                  ->where('pat_id_poliza',$poliza->id_insurancepolicies)
-                                                  ->where('pat_status','=',0)->get();
-                                                  if ( $patologiasno->count() >0)
-                                                  {
-                                                    foreach ($patologiasno as $patologiano => $pn)
+                                                    }
+                                                    else
                                                     {
-                                                      
-                                                      ?>
-                                                          <p>
-                                                          <?php
-                                                            echo $pn->pat_descripcion; 
-                                                          ?>
-                                                          </p>
-                                                            
-                                                      <?php
+                                                      echo " Ninguno ";
                                                     }
+                                                  ?>
+                                                </div>
+                                                <div class ="card m-2">
+                                                  <h6 style ="color:#911d1b !important;">
+                                                    Patologias NO Declaradas
+                                                  </h6>
+                                                  <?php
+                                                    $patologiasno =  DB::table('patologia')
+                                                    ->where('pat_id_poliza',$poliza->id_insurancepolicies)
+                                                    ->where('pat_status','=',0)->get();
+                                                    if ( $patologiasno->count() >0)
+                                                    {
+                                                      foreach ($patologiasno as $patologiano => $pn)
+                                                      {
+                                                        
+                                                        ?>
+                                                            <p>
+                                                            <?php
+                                                              echo $pn->pat_descripcion; 
+                                                            ?>
+                                                            </p>
+                                                              
+                                                        <?php
+                                                      }
 
-                                                  }
-                                                  else
-                                                  {
-                                                    echo " Ninguno ";
-                                                  }
-                                                ?>
-                                              </div>
-                                              <!--  -->
+                                                    }
+                                                    else
+                                                    {
+                                                      echo " Ninguno ";
+                                                    }
+                                                  ?>
+                                                </div>
+                                                <!--  -->
                                             </div>
                                           </div>
                                         </div>
@@ -226,7 +233,83 @@
     </div>
     <!--  --> 
 </div>
-<?php 
-  
-?>
+<script> 
+  let vcabezerapolizas =[];
+  let vcabezerapolizasnotas =[];
+  function clickpoliza(id)
+  {
+    let collapsePoliza = document.getElementById("collapsePoliza_"+id);
+    if (vcabezerapolizas.includes(id))
+    {
+      
+        vcabezerapolizas.shift(); 
+        
+        collapsePoliza.classList.remove('accordion-collapse'); 
+        collapsePoliza.classList.remove('collapse')
+        collapsePoliza.classList.remove('show'); 
+      
+        collapsePoliza.classList.add('accordion-collapse'); 
+        collapsePoliza.classList.add('collapse'); 
+        collapsePoliza.classList.add('hide'); 
+
+        collapsePoliza.style.display = "none"; console.log(' con display ')
+    }
+    else
+    {
+      if (vcabezerapolizas.length >0)
+      {
+          
+          let antes = vcabezerapolizas[0];
+          let anterior =document.getElementById("collapsePoliza_"+antes);
+          anterior.style.display = "none";
+          vcabezerapolizas.shift();
+          vcabezerapolizas.push(id);
+          anterior.style.display = "block";
+      }
+      else
+      {
+          vcabezerapolizas.push(id);
+          collapsePoliza.style.display = "block";
+      }
+    }
+  }
+  function clicknota(id)
+  {
+    let collapseinfonotas = document.getElementById("collapseinfonotas_"+id);
+    if (vcabezerapolizasnotas.includes(id))
+    {
+      
+        vcabezerapolizasnotas.shift(); 
+        
+        collapseinfonotas.classList.remove('accordion-collapse'); 
+        collapseinfonotas.classList.remove('collapse')
+        collapseinfonotas.classList.remove('show'); 
+      
+        collapseinfonotas.classList.add('accordion-collapse'); 
+        collapseinfonotas.classList.add('collapse'); 
+        collapseinfonotas.classList.add('hide'); 
+
+        collapseinfonotas.style.display = "none";
+    }
+    else
+    {
+      if (vcabezerapolizasnotas.length >0)
+      {
+          
+          let antes = vcabezerapolizasnotas[0];
+          let anterior =document.getElementById("collapseinfonotas_"+antes);
+          anterior.style.display = "none";
+          vcabezerapolizasnotas.shift();
+          vcabezerapolizasnotas.push(id);
+          anterior.style.display = "block";
+      }
+      else
+      {
+          vcabezerapolizasnotas.push(id);
+          collapseinfonotas.style.display = "block";
+      }
+    }
+  }
+</script> 
+
 @endsection
